801694884ecf39d24f6ec177875431246f6b1cc8
pancake <pancake@nopcode.org>
2023-12-19 11:08:40 +0100

    Add llama.cpp package

---
+++ db/llama.cpp
@@ -0,0 +1,18 @@
+R2PM_BEGIN
+
+R2PM_GIT https://github.com/ggerganov/llama.cpp
+R2PM_DESC "The most advanced and performant opensource language model implementation"
+
+R2PM_NEEDS "gcc make cmake"
+
+R2PM_INSTALL() {
+	cmake -DCMAKE_INSTALL_PREFIX:PATH=${R2PM_PREFIX} .
+	${MAKE} -j4
+	${R2PM_SUDO} ${MAKE} install
+}
+
+R2PM_UNINSTALL() {
+	${R2PM_SUDO} ${MAKE} uninstall
+}
+
+R2PM_END
